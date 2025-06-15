7bc7ab8d6a8a78f2bd5fc1555f4b34817ab6f954
Parsa Besharat <147280090+parsabe@users.noreply.github.com>
2025-06-15 07:34:07 -0700

    Delete AI directory

--- AI/Ü1/source/E11Fluege1.pl
+++
@@ -1,8 +0,0 @@
-/* flugRoute( Startflughafen, Zielflughafen) */
-flugRoute(dresden, zürich).
-flugRoute(dresden, frankfurt).
-flugRoute(dresden, mallorca).
-flugRoute(dresden, düsseldorf).
-flugRoute(dresden, hunghada).
-flugRoute(dresden, antalya).
-
